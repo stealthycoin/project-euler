793cf30c398874b98c229bfb432931baec57a524
stealthycoin <john.w.carlyle@gmail.com>
2013-11-07 23:55:34 -0800

    Initial commit

---
+++ euler1/euler1.lisp
@@ -0,0 +1,11 @@
+;; Project Euler Problem 1
+;; Author: John Carlyle
+;; Date: 1/27/12
+
+(defun euler1 (limit)
+  (let ((s 0))
+    (dotimes (i limit)
+      (when (or
+	     (zerop (mod i 3))
+	     (zerop (mod i 5)))
+	(setf s (+ s i)))) s))
