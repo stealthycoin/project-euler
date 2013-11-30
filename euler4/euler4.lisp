5ff038457aef024f73288957ec36bcd628b8c05e
StealthyCoin <john.w.carlyle@gmail.com>
2013-11-29 22:09:24 -0800

    Finished 4

--- euler4/euler4.lisp
+++ euler4/euler4.lisp
@@ -1,12 +1,19 @@
-;; Project Euler Problem 3
+;; Project Euler Problem 4
 ;; Author: John Carlyle
-;; Date: 
+;; Date: 11/29/2013
 
 (defun palp (x)
   (equal x (reverse x)))
 
 (defun euler4 (&optional (digits 3))
-  (let* ((min (* 10 digits))
-	 (max (- (* min 10) 1)))
-    
-  
+  (let* ((min (expt 10 (- digits 1)))
+	 (max (- (* min 10) 1))
+	 (largest 0))
+    (loop for i  from min to max do 
+	  (loop for j from min to max do
+		(let ((candidate (* i j)))
+		  (setf largest 
+			(if (palp (write-to-string candidate))
+			    (max candidate largest)
+			  largest)))))
+    largest))
