c564f07dcb0da82c06e0aaf922a79a179ac59923
stealthycoin <john.w.carlyle@gmail.com>
2013-12-18 20:02:04 -0800

    Fixed 5

--- euler5/euler5.lisp
+++ euler5/euler5.lisp
@@ -1,13 +1,26 @@
 ;; Project Euler Problem 5
 ;; Name: John Carlyle
-;; Date: 
+;; Date: 12/18/2013
 
-(defun primep (x)
-  (let ((sqrt-x (sqrt x)))
-    (do ((i 2 (1+ i)))
-        ((> i sqrt-x) t)
-      (when (eq (mod x i) 0)
-	(return nil)))))
+(defun p? (list value) 
+  (if (zerop (length list))
+      t
+    (if (equal (mod value 
+		    (first list)) 
+	       0)
+	(p? (cdr list) value)
+      nil)))
+	
 
+(defun search-for-p-aux (list candidate)
+  (if (p? list candidate)
+      candidate
+    (search-for-p-aux list (1+ candidate))))
+
+(defun search-for-p (list)
+  (search-for-p-aux list 
+		    (first (last list))))
+  	
 (defun euler5 (&optional (limit 20))
-  (apply '* (loop for i from 1 to limit collect i)))
+  (search-for-p (loop for i from 1 to limit collect i)))
+
